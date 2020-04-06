8691eba57621853ee1d2ab334353c85b65e93573
TM <tmangeard@gmx.fr>
2020-04-06 16:08:09 +1100

    add cover substrat and precision 00.0

--- insert_data_shape_frequency_cover.sql
+++ insert_data_shape_frequency_cover.sql
@@ -5,13 +5,13 @@ INSERT INTO niamoto_portal.data_shape_frequency(
 	(select * from (
 	Select 'cover_forest' class_object,
 			'Forêt' class_name,
-		round(forest_area_ha/land_area_ha,2)*100 as class_value,
+		round(forest_area_ha/land_area_ha*100, 1) as class_value,
 		gid shape_id
 		from atlas_pn.pn_emprises
 	Union	
 	Select 'cover_forest' class_object,
 			'Hors-forêt' class_name,
-		round((land_area_ha-forest_area_ha)/land_area_ha, 2)*100 as class_value,
+		round((land_area_ha-forest_area_ha)/land_area_ha*100, 1) as class_value,
 		gid shape_id
 		from atlas_pn.pn_emprises
 ) cover_forest
@@ -23,13 +23,13 @@ INSERT INTO niamoto_portal.data_shape_frequency(
 	(select * from (
 	Select 'cover_forestum' class_object,
 			'Forêt' class_name,
-		Case  when landum_area_ha>0 then round(forest_um_area_ha/landum_area_ha,2)*100 else 0 end as class_value,
+		Case  when land_um_area_ha>0 then round(forest_um_area_ha/land_um_area_ha*100, 1) else 0 end as class_value,
 		gid shape_id
 		from atlas_pn.pn_emprises
 	Union	
 	Select 'cover_forestum' class_object,
 			'Hors-forêt' class_name,
-		Case  when landum_area_ha>0  then round((landum_area_ha-forest_um_area_ha)/landum_area_ha, 2)*100 else 0 end as class_value,
+		Case  when land_um_area_ha>0  then round((land_um_area_ha-forest_um_area_ha)/land_um_area_ha*100, 1) else 0 end as class_value,
 		gid shape_id
 		from atlas_pn.pn_emprises
 ) cover_forestum
@@ -41,13 +41,13 @@ INSERT INTO niamoto_portal.data_shape_frequency(
 	(select * from (
 	Select 'cover_forestnum' class_object,
 			'Forêt' class_name,
-		Case  when (land_area_ha-landum_area_ha)>0 then round((forest_area_ha-forest_um_area_ha)/(land_area_ha-landum_area_ha),2)*100 else 0 end as class_value,
+		Case  when (land_area_ha-land_um_area_ha)>0 then round((forest_area_ha-forest_um_area_ha)/(land_area_ha-land_um_area_ha)*100, 1) else 0 end as class_value,
 		gid shape_id
 		from atlas_pn.pn_emprises
 	Union	
 	Select 'cover_forestnum' class_object,
 			'Hors-forêt' class_name,
-		Case  when (land_area_ha-landum_area_ha)>0  then round(((land_area_ha-forest_area_ha)-(landum_area_ha-forest_um_area_ha))/(land_area_ha-landum_area_ha), 2)*100 else 0 end as class_value,
+		Case  when (land_area_ha-land_um_area_ha)>0  then round(((land_area_ha-forest_area_ha)-(land_um_area_ha-forest_um_area_ha))/(land_area_ha-land_um_area_ha)*100, 1) else 0 end as class_value,
 		gid shape_id
 		from atlas_pn.pn_emprises
 ) cover_forestnum
@@ -59,46 +59,64 @@ INSERT INTO niamoto_portal.data_shape_frequency(
 	(select * from (
 	Select 'cover_foresttype' class_object,
 			'Forêt secondaire' class_name,
-		case when forest_area_ha>0 then round(((forest_area_ha -forest_100m_ha)/forest_area_ha)::numeric,2)*100 else 0 end as forest_core,
+		case when forest_area_ha>0 then round(((forest_area_ha -forest_100m_ha)/forest_area_ha)::numeric*100, 1) else 0 end as forest_core,
 		gid shape_id
 		from atlas_pn.pn_emprises
 	Union	
 	Select 'cover_foresttype' class_object,
 			'Forêt mature' class_name,
-		case when forest_area_ha>0 then round(((forest_100m_ha-forest_ssdm80_ha)/forest_area_ha)::numeric,2)*100 else 0 end as  forest_mature,
+		case when forest_area_ha>0 then round(((forest_100m_ha-forest_ssdm80_ha)/forest_area_ha)::numeric*100, 1) else 0 end as  forest_mature,
 		gid shape_id
 		from atlas_pn.pn_emprises
 	Union
 	Select 'cover_foresttype' class_object,
 			'Forêt coeur' class_name,
-		case when forest_area_ha>0 then round((forest_ssdm80_ha/forest_area_ha)::numeric,2)*100 else 0 end as forest_coeur,
+		case when forest_area_ha>0 then round((forest_ssdm80_ha/forest_area_ha)::numeric*100, 1) else 0 end as forest_coeur,
 		gid shape_id
 		from atlas_pn.pn_emprises
 ) cover_foresttype
 order by shape_id,class_object, class_name);
 
 
-------- admin forest cover --------------------------------------
 INSERT INTO niamoto_portal.data_shape_frequency(
+------- admin forest cover --------------------------------------
 	class_object, class_name, class_value, shape_id)
 	(select * from (
 	Select 'cover_forestadmin' class_object,
 		'Concession' class_name,
-		case when forest_area_ha>0 then round(forest_mining_ha/forest_area_ha,3) * 100 else 0 end as forest,
+		case when forest_area_ha>0 then round(forest_mining_ha/forest_area_ha*100, 1) else 0 end as forest,
 		gid shape_id
 		from atlas_pn.pn_emprises where type='Commune'
 	Union
 	Select 'cover_forestadmin' class_object,
 		'Réserves' class_name,
-		case when forest_area_ha>0 then round(forest_reserve_ha/forest_area_ha,3) * 100 else 0 end as forest,
+		case when forest_area_ha>0 then round(forest_reserve_ha/forest_area_ha*100, 1) else 0 end as forest,
 		gid shape_id
 		from atlas_pn.pn_emprises where type='Commune'
 	Union
 	Select 'cover_forestadmin' class_object,
 		'Forêt' class_name,
-		case when forest_area_ha>0 then round((forest_area_ha - forest_reserve_ha - forest_mining_ha)/forest_area_ha,3) *100 else 0 end as forest,
+		case when forest_area_ha>0 then round((forest_area_ha - forest_reserve_ha - forest_mining_ha)/forest_area_ha*100, 1) else 0 end as forest,
 		gid shape_id
 		from atlas_pn.pn_emprises where type='Commune'
 	
 ) cover_forestadmin
 order by shape_id, class_object, forest);
+
+--------- substrat ------------------------------
+INSERT INTO niamoto_portal.data_shape_frequency(
+	class_object, class_name, class_value, shape_id)
+	(select * from (
+	Select 'substrat' class_object,
+			'UM' class_name,
+		case when land_um_area_ha is not null then round((land_um_area_ha)/land_area_ha*100, 1) else 0 end class_value,
+		gid shape_id
+		from atlas_pn.pn_emprises
+	Union	
+	Select 'substrat' class_object,
+			'NUM' class_name,
+		case when land_um_area_ha is not null then round((land_area_ha-land_um_area_ha)/land_area_ha*100, 1) else 0 end class_value,
+		gid shape_id
+		from atlas_pn.pn_emprises
+) substrat
+order by shape_id,class_object, class_name);
