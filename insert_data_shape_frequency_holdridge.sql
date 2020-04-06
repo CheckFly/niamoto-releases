90cfcb7f3ddcd131ae0690cd314fb970b8b3ffd9
TM <tmangeard@gmx.fr>
2020-04-06 16:03:23 +1100

    add classe sl and sort cl, precision numeric:3

--- insert_data_shape_frequency_holdridge.sql
+++ insert_data_shape_frequency_holdridge.sql
@@ -1,7 +1,7 @@
 with 
 	hodlridge_classif as (
 		select * from (
-			select gid_emprise,mnt_object,'Très sec' classe, sum(pixelcount) pixelcount from atlas_pn.pn_emprises_holdridge
+			select gid_emprise,mnt_object,'Sec' classe, sum(pixelcount) pixelcount from atlas_pn.pn_emprises_holdridge
 			where classe in ('1','2') and mnt_object in ('forest', 'land')
 			group by 1,2,3
 			union
@@ -11,8 +11,7 @@ with
 			union
 			select gid_emprise,mnt_object,'Très humide'classe, sum(pixelcount) pixelcount from atlas_pn.pn_emprises_holdridge
 			where classe in ('4','5') and mnt_object in ('forest', 'land')
-			group by 1,2,3) as hodlridge_classif
-		order by 1,2,3),
+			group by 1,2,3) as hodlridge_classif),
 	shape_land_all_pixel as (
 		SELECT gid_emprise, type,name, sum(pixelcount) allpixel
 					FROM  atlas_pn.pn_emprises a
@@ -23,23 +22,23 @@ with
 INSERT INTO niamoto_portal.data_shape_frequency(class_object, class_name, class_value, shape_id)
 	SELECT concat('holdridge_', hc.mnt_object) class_object,
 		hc.classe, 
-		Case when hc.pixelcount >0 then round((hc.pixelcount::float/sap.allpixel)::numeric,2) else 0 end  class_value,
+		Case when hc.pixelcount >0 then round((hc.pixelcount::float/sap.allpixel)::numeric,3) else 0 end  class_value,
 		hc.gid_emprise shape_id 
 	from hodlridge_classif hc
 	left join shape_land_all_pixel sap on hc.gid_emprise=sap.gid_emprise
-	order by shape_id
+
 
 UNION ALL
 
-INSERT INTO niamoto_portal.data_shape_frequency(class_object, class_name, class_value, shape_id)
+
 	SELECT 'holdridge_forest_out' class_object,
 		hc.classe,
-		Case when (hc.pixelcount-hc_forest.pixelcount) >0 then round(((hc.pixelcount-hc_forest.pixelcount)::float/sap.allpixel)::numeric,2) else 0 end  class_value,
+		Case when (hc.pixelcount-hc_forest.pixelcount) >0 then round(((hc.pixelcount-hc_forest.pixelcount)::float/sap.allpixel)::numeric,3) else 0 end  class_value,
 		hc.gid_emprise shape_id 
 	from hodlridge_classif hc
 	left join shape_land_all_pixel sap on hc.gid_emprise=sap.gid_emprise and hc.mnt_object = 'land' 
 	left join hodlridge_classif hc_forest on hc.gid_emprise=hc_forest.gid_emprise and hc_forest.mnt_object='forest' and hc.classe=hc_forest.classe
 	where hc.mnt_object='land'
-	order by shape_id
+
 
 
