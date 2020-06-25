76a194fd87e388d09f05ae7d643ce4802d6cc755
TM <tmangeard@gmx.fr>
2020-06-25 14:02:49 +1100

    fix name path data move

--- update.sh
+++ update.sh
@@ -61,7 +61,7 @@ traitement(){
         echo "************************* Push data *************************************************"
         # connect virtualenv
         sudo docker exec niamoto-django-local_niamoto-django_1 bash generate_data.sh
-        sudo mv /home/niamoto_portal/data/data.json /home/niamoto_portal/
+        sudo mv /home/niamoto-portal/data/data.json /home/niamoto-portal/
         sshpass -p $SSHPASSWORD scp -P $PORT /home/niamoto-portal/data/data.json niamoto@niamoto.ddns.net:/home/niamoto
         sshpass -p $SSHPASSWORD ssh -p $PORT niamoto@niamoto.ddns.net sudo mv /home/niamoto/data.json /home/niamoto/data
         sshpass -p $SSHPASSWORD ssh -p $PORT niamoto@niamoto.ddns.net sudo bash /home/niamoto/update.niamoto-docker.sh
