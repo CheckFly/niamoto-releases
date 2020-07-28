5f3044f3f9aa44273e7694f25025ec289fc8b969
TM <tmangeard@gmx.fr>
2020-07-28 11:45:02 +1100

    update data holdridge

--- niamoto_portal/function_insert_taxon_frequency.sql
+++ niamoto_portal/function_insert_taxon_frequency.sql
@@ -17,6 +17,10 @@ with
 strate_total as (SELECT taxon_id, class_object, case when sum(class_value)>0 then sum(class_value) else 1 end  total
 	FROM niamoto_preprocess.data_taxon_frequency
 	where class_object = 'strate'
+	GROUP BY taxon_id, class_object),
+holdridge_total as (SELECT taxon_id, class_object, case when sum(class_value)>0 then sum(class_value) else 1 end  total
+	FROM niamoto_preprocess.data_taxon_frequency
+	where class_object = 'holdridge'
 	GROUP BY taxon_id, class_object)
 
 INSERT INTO niamoto_portal.data_taxon_frequency (taxon_id, class_object, class_name, class_value, param3_float) 				  
@@ -27,9 +31,16 @@ INSERT INTO niamoto_portal.data_taxon_frequency (taxon_id, class_object, class_n
 
 UNION ALL
 
+SELECT dtf.taxon_id, dtf.class_object, dtf.class_name, round((dtf.class_value/ht.total)::numeric,2)*100 class_value, dtf.class_index
+	FROM niamoto_preprocess.data_taxon_frequency dtf
+    LEFT JOIN holdridge_total ht ON dtf.taxon_id=ht.taxon_id
+	where dtf.class_object = 'holdridge'
+
+UNION ALL
+
 SELECT dtf.taxon_id, dtf.class_object, dtf.class_name, dtf.class_value, dtf.class_index
 	FROM niamoto_preprocess.data_taxon_frequency dtf
-	where dtf.class_object in ('dbh', 'top_species', 'holdridge', 'rainfall', 'elevation' ));
+	where dtf.class_object in ('dbh', 'top_species', 'rainfall', 'elevation' ));
 	
 
 	
