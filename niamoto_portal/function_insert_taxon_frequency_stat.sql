2a91f315665601ad821d608faffea33f704d5c4b
TM <tmangeard@gmx.fr>
2020-08-13 08:49:26 +1100

    delete script holdridge

--- niamoto_portal/function_insert_taxon_frequency_stat.sql
+++ niamoto_portal/function_insert_taxon_frequency_stat.sql
@@ -2,7 +2,7 @@
 
 -- DROP FUNCTION niamoto_portal.insert_taxon_frequency_phenology();
 
-CREATE OR REPLACE FUNCTION niamoto_portal.insert_taxon_frequency(
+CREATE OR REPLACE FUNCTION niamoto_portal.insert_taxon_frequency_stat(
 	)
     RETURNS integer
     LANGUAGE 'plpgsql'
@@ -16,14 +16,8 @@ AS $BODY$
 
 INSERT INTO niamoto_portal.data_taxon_frequency (taxon_id, class_object, class_name, class_value) 				  
 (SELECT  1, 'stats', 'sum_occ', sum(occ_count)
-	FROM niamoto_preprocess.data_taxon_taxon where id_rang=10;
+	FROM niamoto_preprocess.data_taxon_taxon where id_rang=10
 
-UNION ALL
-
-SELECT dtf.taxon_id, dtf.class_object, dtf.class_name, round((dtf.class_value/ht.total)::numeric,2)*100 class_value, dtf.class_index
-	FROM niamoto_preprocess.data_taxon_frequency dtf
-    LEFT JOIN holdridge_total ht ON dtf.taxon_id=ht.taxon_id
-	where dtf.class_object = 'holdridge'
 
 
 );
@@ -33,5 +27,5 @@ SELECT dtf.taxon_id, dtf.class_object, dtf.class_name, round((dtf.class_value/ht
         END;
 $BODY$;
 
-ALTER FUNCTION niamoto_portal.insert_taxon_frequency()
+ALTER FUNCTION niamoto_portal.insert_taxon_frequency_stat()
     OWNER TO amapiac;
