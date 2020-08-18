df6779974352ef5d6a2064a137f573a1a81958b8
TM <tmangeard@gmx.fr>
2020-08-18 16:25:44 +1100

    fix call function

--- niamoto_portal/function_insert_data.sql
+++ niamoto_portal/function_insert_data.sql
@@ -23,7 +23,7 @@ DECLARE passed BOOLEAN;
                 SELECT niamoto_portal.insert_taxon_frequency() INTO passed;
                 SELECT niamoto_portal.insert_taxon_frequency_stat() INTO passed;
                 SELECT niamoto_portal.insert_plot() INTO passed;
-                SELECT niamoto_portal.insert_plot_frequency INTO passed;
+                SELECT niamoto_portal.insert_plot_frequency() INTO passed;
                 SELECT niamoto_portal.insert_list_tree() INTO passed;
                 RETURN 1;
                 
