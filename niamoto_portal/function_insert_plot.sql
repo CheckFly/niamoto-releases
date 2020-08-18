81003cd893230ee392a108e5b460e36006c59996
TM <tmangeard@gmx.fr>
2020-08-18 16:20:22 +1100

    update script plot

--- niamoto_portal/function_insert_plot.sql
+++ niamoto_portal/function_insert_plot.sql
@@ -15,9 +15,9 @@ AS $BODY$
 
           TRUNCATE niamoto_portal.data_plot_plot CASCADE;
           INSERT INTO niamoto_portal.data_plot_plot(
-	id, label, width, height, location, elevation, species_level, total_stems, living_stems, count_families, count_species, shannon, pielou, simpson, basal_area, h_mean, dbh_mean, dbh_median, dbh_min, dbh_max, wood_density, biomasse)
-	SELECT id_locality, locality, plot_type, species_level, total_stems, living_stems, nb_families, nb_species, shannon, pielou, simpson, basal_area, h_mean, dbh_mean, dbh_median, dbh_min, dbh_max, ferns, palms, lianas, emergent, canopy, undercanopy, understorey, strate_indet
-                FROM niamoto_preprocess.data_plot_plot;
+                           id, label,location, species_level, total_stems, living_stems, count_families, count_species, shannon, pielou, simpson, basal_area, h_mean, dbh_mean, dbh_median, dbh_min, dbh_max)
+        SELECT id_locality, locality,location, species_level, total_stems, living_stems, nb_families, nb_species, shannon, pielou, simpson, basal_area, h_mean, dbh_mean, dbh_median, dbh_min, dbh_max
+        FROM niamoto_preprocess.data_plot_plot dpp;
           
           RETURN 1;
         END;
