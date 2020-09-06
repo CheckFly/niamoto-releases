c6783da15288113c6a1f32993818bc045415ac18
TM <tmangeard@gmx.fr>
2020-09-07 08:07:30 +1100

    inser data speciesTop10 and familyTop10

--- niamoto_portal/function_insert_plot.sql
+++ niamoto_portal/function_insert_plot.sql
@@ -17,7 +17,8 @@ AS $BODY$
           INSERT INTO niamoto_portal.data_plot_plot(
                            id, label,location, species_level, total_stems, living_stems, count_families, count_species, shannon, pielou, simpson, basal_area, h_mean, dbh_mean, dbh_median, dbh_min, dbh_max)
         SELECT id_locality, locality,location, species_level, total_stems, living_stems, nb_families, nb_species, shannon, pielou, simpson, basal_area, h_mean, dbh_mean, dbh_median, dbh_min, dbh_max
-        FROM niamoto_preprocess.data_plot_plot dpp;
+        FROM niamoto_preprocess.data_plot_plot dpp
+        WHERE plot_type = 'plot_100x100';
           
           RETURN 1;
         END;
--- niamoto_portal/function_insert_plot_frequency.sql
+++ niamoto_portal/function_insert_plot_frequency.sql
@@ -13,29 +13,55 @@ AS $BODY$
 
         BEGIN
 
-        ALTER SEQUENCE data_plot_frequency_id_seq RESTART WITH 1
+        WITH
+            familyTop10Total AS (
+                SELECT plot_id, SUM(class_value) total
+                FROM niamoto_preprocess.data_plot_frequency 
+                WHERE class_object='familyTop10'
+                GROUP BY plot_id
+            ),
+            speciesTop10Total AS (
+                SELECT plot_id, SUM(class_value) total
+                FROM niamoto_preprocess.data_plot_frequency 
+                WHERE class_object='speciesTop10'
+                GROUP BY plot_id
+            )
+
+
         INSERT INTO niamoto_portal.data_plot_frequency (class_object,class_name, class_value, plot_id, param3_float)
 
         select * from (
-        select 'stems' class_object, 'vivante' classname, round(living_stems/total_stems::numeric,2)*100 class_value, id_locality plot_id,2 i from niamoto_preprocess.data_plot_plot
+        select 'stems' class_object, 'vivante' classname, round(living_stems/total_stems::numeric,2)*100 class_value, id_locality plot_id,2 i from niamoto_preprocess.data_plot_plot WHERE exists (select 1 from niamoto_portal.data_plot_plot WHERE id = id_locality)
         union ALL
-        select 'stems'  class_object, 'morte' classname,round((total_stems - living_stems)/total_stems::numeric,2)*100 class_value, id_locality plot_id,1 i from niamoto_preprocess.data_plot_plot
+        select 'stems'  class_object, 'morte' classname,round((total_stems - living_stems)/total_stems::numeric,2)*100 class_value, id_locality plot_id,1 i from niamoto_preprocess.data_plot_plot WHERE exists (select 1 from niamoto_portal.data_plot_plot WHERE id = id_locality)
         UNION ALL
-        select 'type_plant'  class_object, 'arbres' classname,round(total_stems/(ferns+palms+lianas+total_stems)::numeric,2)*100 class_value, id_locality plot_id,1 i from niamoto_preprocess.data_plot_plot
+        select 'type_plant'  class_object, 'arbres' classname,round(total_stems/(ferns+palms+lianas+total_stems)::numeric,2)*100 class_value, id_locality plot_id,1 i from niamoto_preprocess.data_plot_plot WHERE exists (select 1 from niamoto_portal.data_plot_plot WHERE id = id_locality)
         UNION ALL
-        select 'type_plant'  class_object, 'fougères' classname,round(ferns/(ferns+palms+lianas+total_stems)::numeric,2)*100 class_value, id_locality plot_id,2 i from niamoto_preprocess.data_plot_plot
+        select 'type_plant'  class_object, 'fougères' classname,round(ferns/(ferns+palms+lianas+total_stems)::numeric,2)*100 class_value, id_locality plot_id,2 i from niamoto_preprocess.data_plot_plot WHERE exists (select 1 from niamoto_portal.data_plot_plot WHERE id = id_locality)
         UNION ALL
-        select 'type_plant'  class_object, 'palmiers' classname,round(palms/(ferns+palms+lianas+total_stems)::numeric,2)*100 class_value, id_locality plot_id,3 i from niamoto_preprocess.data_plot_plot
+        select 'type_plant'  class_object, 'palmiers' classname,round(palms/(ferns+palms+lianas+total_stems)::numeric,2)*100 class_value, id_locality plot_id,3 i from niamoto_preprocess.data_plot_plot WHERE exists (select 1 from niamoto_portal.data_plot_plot WHERE id = id_locality)
         UNION ALL
-        select 'type_plant'  class_object, 'lianes' classname,round(lianas/(ferns+palms+lianas+total_stems)::numeric,2)*100 class_value, id_locality plot_id,4 i from niamoto_preprocess.data_plot_plot
+        select 'type_plant'  class_object, 'lianes' classname,round(lianas/(ferns+palms+lianas+total_stems)::numeric,2)*100 class_value, id_locality plot_id,4 i from niamoto_preprocess.data_plot_plot WHERE exists (select 1 from niamoto_portal.data_plot_plot WHERE id = id_locality)
         UNION ALL
-        select 'strates'  class_object, 'emergant' classname,round(emergent/(living_stems)::numeric,2)*100 class_value, id_locality plot_id,1 i from niamoto_preprocess.data_plot_plot
+        select 'strates'  class_object, 'emergant' classname,round(emergent/(living_stems)::numeric,2)*100 class_value, id_locality plot_id,1 i from niamoto_preprocess.data_plot_plot WHERE exists (select 1 from niamoto_portal.data_plot_plot WHERE id = id_locality)
         UNION ALL
-        select 'strates'  class_object, 'canopée' classname,round(canopy/(living_stems)::numeric,2)*100 class_value, id_locality plot_id,2 i from niamoto_preprocess.data_plot_plot
+        select 'strates'  class_object, 'canopée' classname,round(canopy/(living_stems)::numeric,2)*100 class_value, id_locality plot_id,2 i from niamoto_preprocess.data_plot_plot WHERE exists (select 1 from niamoto_portal.data_plot_plot WHERE id = id_locality)
         UNION ALL
-        select 'strates'  class_object, 'sous-canopée' classname,round(undercanopy/(living_stems)::numeric,2)*100 class_value, id_locality plot_id,3 i from niamoto_preprocess.data_plot_plot
+        select 'strates'  class_object, 'sous-canopée' classname,round(undercanopy/(living_stems)::numeric,2)*100 class_value, id_locality plot_id,3 i from niamoto_preprocess.data_plot_plot WHERE exists (select 1 from niamoto_portal.data_plot_plot WHERE id = id_locality)
         UNION ALL
-        select 'strates'  class_object, 'sous-bois' classname,round(understorey/(living_stems)::numeric,2)*100 class_value, id_locality plot_id,4 i from niamoto_preprocess.data_plot_plot
+        select 'strates'  class_object, 'sous-bois' classname,round(understorey/(living_stems)::numeric,2)*100 class_value, id_locality plot_id,4 i from niamoto_preprocess.data_plot_plot WHERE exists (select 1 from niamoto_portal.data_plot_plot WHERE id = id_locality)
+        UNION  ALL
+        select class_object, class_name, round((class_value/total)::numeric,2)*100 class_value, dpf.plot_id, param3_float
+		from niamoto_preprocess.data_plot_frequency dpf
+		LEFT JOIN familyTop10Total ftt ON dpf.plot_id = ftt.plot_id
+		WHERE exists (select 1 from niamoto_portal.data_plot_plot WHERE id = dpf.plot_id)
+            AND class_object ='familyTop10'
+        UNION ALL
+        select class_object, class_name, round((class_value/total)::numeric,2)*100 class_value, dpf.plot_id,param3_float
+		from niamoto_preprocess.data_plot_frequency dpf
+		LEFT JOIN speciesTop10Total ftt ON dpf.plot_id = ftt.plot_id
+		WHERE exists (select 1 from niamoto_portal.data_plot_plot WHERE id = dpf.plot_id)
+			AND class_object='speciesTop10'
         ) as t order by 4,1,5;
 
         RETURN 1;
