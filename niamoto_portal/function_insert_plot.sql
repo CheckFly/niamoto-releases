96497692d7a9475771c1c4a2243b17486a276f17
TM <tmangeard@gmx.fr>
2020-09-17 08:33:36 +1100

    update query add condition province

--- niamoto_portal/function_insert_plot.sql
+++ niamoto_portal/function_insert_plot.sql
@@ -15,10 +15,10 @@ AS $BODY$
 
           TRUNCATE niamoto_portal.data_plot_plot CASCADE;
           INSERT INTO niamoto_portal.data_plot_plot(
-                           id, label,location, species_level, total_stems, living_stems, count_families, count_species, shannon, pielou, simpson, basal_area, h_mean, dbh_mean, dbh_median, dbh_min, dbh_max)
-        SELECT id_locality, locality,location, species_level, total_stems, living_stems, nb_families, nb_species, shannon, pielou, simpson, basal_area, h_mean, dbh_mean, dbh_median, dbh_min, dbh_max
+                           id, label,location, species_level, elevation, total_stems, living_stems, count_families, count_species, shannon, pielou, simpson, basal_area, h_mean, dbh_mean, dbh_median, dbh_min, dbh_max,longitude, latitude, holdridge, rainfall, um_substrat, town)
+        SELECT id_locality, locality,geo_pt, species_level, elevation, total_stems, living_stems, nb_families, nb_species, shannon, pielou, simpson, basal_area, h_mean, dbh_mean, dbh_median, dbh_min, dbh_max, longitude, latitude, holdridge, rainfall, um_substrat, commune
         FROM niamoto_preprocess.data_plot_plot dpp
-        WHERE plot_type = 'plot_100x100';
+        WHERE plot_type = 'plot_100x100' AND province='PN';
           
           RETURN 1;
         END;
