eb1a2d8abc676c49f6cbcd021fd61ad9813e9932
TM <tmangeard@gmx.fr>
2020-11-10 17:44:03 +1100

    update sql ressources

--- niamoto_portal/function_insert_portal_ressources.sql
+++ niamoto_portal/function_insert_portal_ressources.sql
@@ -14,9 +14,9 @@ AS $BODY$
         BEGIN
 
             TRUNCATE niamoto_portal.portal_ressource CASCADE;
-            ALTER SEQUENCE portal_ressource_id_seq RESTART WITH 1;
+            ALTER SEQUENCE niamoto_portal.portal_ressource_id_seq RESTART WITH 1;
             INSERT INTO niamoto_portal.portal_ressource (support, who, description, journal, issue, pages, year, link)
-            SELECT support, who, description, journal, issue, pages, year, link
+            SELECT support, authors, title, journal, issue, pages, year, link
             FROM data_preprocess.ressources;
           
           RETURN 1;
