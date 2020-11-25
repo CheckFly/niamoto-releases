f66e3429c62af2ca0ccdae79691692bbe72098cb
TM <tmangeard@gmx.fr>
2020-11-25 14:38:29 +1100

    add field category in ressource

--- niamoto_portal/function_insert_portal_ressources.sql
+++ niamoto_portal/function_insert_portal_ressources.sql
@@ -15,8 +15,8 @@ AS $BODY$
 
             TRUNCATE niamoto_portal.portal_ressource CASCADE;
             ALTER SEQUENCE niamoto_portal.portal_ressource_id_seq RESTART WITH 1;
-            INSERT INTO niamoto_portal.portal_ressource (support, who, description, journal, issue, pages, year, link)
-            SELECT support, authors, title, journal, issue, pages, year, link
+            INSERT INTO niamoto_portal.portal_ressource (support, who, description, category, journal, issue, pages, year, link)
+            SELECT support, authors, title, category, journal, issue, pages, year, link
             FROM data_preprocess.ressources;
           
           RETURN 1;
